df6729f0170602f8fb1a08cb97bdae52a1a7bee0
Victoria B <viktorija.bourassa@gmail.com>
2019-01-14 19:03:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -57,5 +57,9 @@ end
  end
 
  def add_s(array)
-   array.each_with_index.collect {|element, index| element + "s" if index != 1 }
+   array.each_with_index.collect do |element, index| if index != 1  
+    element + "s" 
+    else element 
+    end
+    end
 end
