343eff066dccbe105eeb801cb5605e2f740d74cb
Victoria B <viktorija.bourassa@gmail.com>
2019-01-14 19:04:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,9 +1,9 @@
 def sort_array_asc(array)
-  return array.sort
+  array.sort
 end
 
 def sort_array_desc(array)
-  return array.sort.reverse
+  array.sort.reverse
 end
 
 def sort_array_char_count(array)
