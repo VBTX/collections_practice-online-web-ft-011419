01c3efb5affe818ebc06a31171b87ffe52a94397
Victoria B <viktorija.bourassa@gmail.com>
2019-01-14 18:25:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,5 +9,21 @@ end
 def sort_array_desc
 end
 
-def swap_elements
+def swap_elements(array)
+  array.sort
+end
+
+def reverse_array
+end
+
+def kesha_maker
+end
+
+def find_a
+end
+
+ def sum_array
+ end
+
+ def add_s
 end
